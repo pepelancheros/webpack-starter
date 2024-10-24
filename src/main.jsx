45c6587ae2a423cf7d5c0ad476e9c3da954f2404
Sebastian Lancheros <sebastian.lancheros@proper.ai>
2024-10-24 17:48:00 -0500

    add react config

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/main.scss";
 
+window.React = React;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div>hola mundo</div>
